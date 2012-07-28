53962278aebdbeb09e290602bdd7eeb4089329eb
Rob Halff <rob.halff@gmail.com>
2012-07-28 13:45:21 +0200

    Add basic test files

--- test/seed_dump_test.rb
+++ test/seed_dump_test.rb
@@ -1,8 +1,27 @@
 require 'test_helper'
+require "seed_dump/perform"
 
 class SeedDumpTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "test WITH_ID" do
-    assert false 
+
+  setup do
+    @sd = SeedDump::Perform.new
+    # universial options for every test
+    @env = {
+     "MODEL_DIR" => Dir.pwd + '/test/models/*.rb',
+     "FILE" => Dir.pwd + '/test/db/seeds.rb',
+     "DEBUG" => true
+    }
+  end
+
+  test "load sample model" do
+    @sd.setup @env 
+    @sd.loadModels
+    assert_equal ["Sample"], @sd.models
+  end
+
+  test "skip callbacks" do
+    @sd.setup @env 
+    @sd.loadModels
+    assert_equal ["Sample"], @sd.models
   end
 end
