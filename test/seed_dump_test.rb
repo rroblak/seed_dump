788a6302032c3f611d14392d27a6c9332082dee2
Rob Halff <rob.halff@gmail.com>
2012-08-01 23:37:35 +0200

    update tests

--- test/seed_dump_test.rb
+++ test/seed_dump_test.rb
@@ -7,24 +7,53 @@ class SeedDumpTest < ActiveSupport::TestCase
     @sd = SeedDump::Perform.new
     # universial options for every test
     @env = {
-     "MODEL_DIR" => Dir.pwd + '/test/models/**.rb',
+     "MODEL_DIR" => 'test/models/**.rb',
      "FILE" => Dir.pwd + '/test/db/seeds.rb',
-     "DEBUG" => true
+     "VERBOSE" => false,
+     "DEBUG" => false 
     }
   end
 
   test "load sample model" do
-    @env['MODEL_DIR'] = Dir.pwd + '/test/models/*.rb'
+    @env['MODEL_DIR'] = 'test/models/*.rb'
     @sd.setup @env 
     @sd.loadModels
     assert_equal ["Sample"], @sd.models
   end
 
   test "support nested models" do
-    @env['MODEL_DIR'] = Dir.pwd + '/test/models/**/*.rb'
+    @env['MODEL_DIR'] = 'test/models/**/*.rb'
     @sd.setup @env 
     @sd.loadModels
     assert_equal ["Nested::Sample", "Sample"], @sd.models
   end
+
+  test "without timestamps" do
+    @env['MODEL_DIR'] = 'test/models/*.rb'
+    @env['TIMESTAMPS'] = false
+    @sd.setup @env
+    @sd.loadModels
+    @sd.dumpModels
+    assert !@sd.last_record.include?("created_at"), "Should not include created_at if timestamps are off"
+  end
+
+  test "with timestamps" do
+    @env['MODEL_DIR'] = 'test/models/*.rb'
+    @env['TIMESTAMPS'] = true
+    @sd.setup @env
+    @sd.loadModels
+    @sd.dumpModels
+    assert @sd.last_record.include?("created_at"), "Must include created_at if timestamps are desired"
+  end
+
+  test "with id" do
+    @env['MODEL_DIR'] = 'test/models/*.rb'
+    @env['WITH_ID'] = true
+    @sd.setup @env
+    @sd.loadModels
+    @sd.dumpModels
+    assert @sd.last_record.include?("id"), "WITH_ID must include id"
+  end
+
 
 end
