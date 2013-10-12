900db31b733c06330ac8f001696a829aafbe614e
Ryan Oblak <rroblak@gmail.com>
2013-10-12 14:25:44 -0700

    Make specs transactional.

--- Gemfile
+++ Gemfile
@@ -14,4 +14,5 @@ end
 group :test do
   gem 'rspec'
   gem 'sqlite3'
+  gem 'database_cleaner'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,10 @@ require 'active_support/core_ext/string'
 require 'active_support/descendants_tracker'
 require 'active_record'
 require 'byebug'
+require 'database_cleaner'
 require './spec/helpers'
+
+ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ':memory:')
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
@@ -11,4 +14,17 @@ RSpec.configure do |config|
   config.order = 'random'
 
   config.include Helpers
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
